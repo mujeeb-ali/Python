3d209d39631e372ab3328913aee35fdb2ed4b9c7
Mujeeb Ur Rehman Shah <mujeebalishah147@gmail.com>
2025-09-17 10:26:43 +0500

    Update trimmedmean.py

--- Python/DSAPython/trimmedmean.py
+++ Python/DSAPython/trimmedmean.py
@@ -8,7 +8,18 @@ print(np.sum(np.array(response_times) / 11))
 Mean = np.mean(response_times)
 Trimmed_Mean = trim_mean(response_times, 0.3)  # Trim 30% of data from each end
 print("Mean:", Mean)
-print(" 30% Trimmed Mean:", Trimmed_Mean)
+print("30% Trimmed Mean:", Trimmed_Mean)
 
+# import numpy as np
+
+# Student scores
+scores = [85, 90, 72, 88]
+
+# Weights (20% assignment, 20% quiz, 30% midterm, 30% final)
+weights = [0.2, 0.2, 0.3, 0.3]
+
+# Weighted mean
+weighted_mean = np.average(scores, weights=weights)
+print(weighted_mean)
 
 
